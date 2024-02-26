b56ef45f842cc82a755004a57a101a1f7f8e1d2f
NguyenTanLap <135099342+tannguyennn@users.noreply.github.com>
2024-02-26 15:23:10 +0700

    upload

---
+++ b1.1.py
@@ -0,0 +1,29 @@
+# viết chương trình pythong nhập 1 số nguyên n thỏa 0<n<=1000
+# lặp lại việc nhập cho đến khi n thỏa
+# kiểm tra n có phải số nguyên tố hay ko
+# in ra các số nguyên tố <=n
+
+
+while(True):
+    n =int(input("Nhập số nguyên n (0<n<=1000): ")) 
+    if n>0 & n<=1000:
+        break
+
+def laSNT(n):
+    if n<2:
+        return False
+    for i in range(2, n//2+1):
+        if n % i == 0 :
+            return False
+    return True
+
+
+if laSNT(n):
+    print("%d là số nguyên tố" % n)
+else:
+    print("%d không là số nguyên tố" % n)
+
+for i in range(2,n+1):
+    if laSNT(i):
+        print("%3d" % i , end=" ")
+
